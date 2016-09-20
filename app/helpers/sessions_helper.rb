54cfb3027d95acebc3a6c951d122ce05716ddd9b
Gabri Doga <bestsuperflycode@gmail.com>
2016-09-20 16:28:26 +0200

    update
    Update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
+      raise       # The tests still pass, so this branch is currently untested.
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -16,6 +18,14 @@ module SessionsHelper
       end
     end
   end
+
+
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
+
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
@@ -28,7 +38,22 @@ module SessionsHelper
   end
 # Logs out the current user.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919092921) do
+ActiveRecord::Schema.define(version: 20160920134950) do
+
+  create_table "tests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -19,6 +26,8 @@ ActiveRecord::Schema.define(version: 20160919092921) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
 end
